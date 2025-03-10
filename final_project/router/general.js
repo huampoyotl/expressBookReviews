4032413b815f1355192627a0ab49e8b7e19cb781
Andres Barrios <andres@generaweb.net>
2025-03-10 19:15:03 -0400

    task 10 - 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,7 @@
 const express = require('express');
-let books = require("./booksdb.js");
+const axios = require('axios');
+let books = {};
+let booksdb = require('./booksdb.js');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -22,43 +24,75 @@ public_users.post("/register", (req,res) => {
 
 });
 
+public_users.get("/db",function(req,res){
+    return res.status(200).json(booksdb);
+})
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+    axios.get('https://andres-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/db')
+        .then(response => {
+            books=response.data
+            return res.status(200).json(books);
+        })
+        .catch(error => {
+            return res.status(500).json({message: "Internal error at getting db", error: error});
+        });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     let ISBN=req.params.isbn;
-    let book=Object.keys(books).filter(i => books[i].ISBN===ISBN);
-    if(book.length>0){
-        return res.status(200).json(books[book[0]]);
-    }else{
-        return res.status(400).json({message: "Book not found, given ISBN: "+ISBN});
-    }
-    
+    axios.get('https://andres-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/db')
+        .then(response => {
+            books=response.data
+            let book=Object.keys(books).filter(i => books[i].ISBN===ISBN);
+            if(book.length>0){
+                return res.status(200).json(books[book[0]]);
+            }else{
+                return res.status(400).json({message: "Book not found, given ISBN: "+ISBN});
+            }
+        })
+        .catch(error => {
+            return res.status(500).json({message: "Internal error at getting db", error: error});
+        });
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     let author=req.params.author;
-    let book=Object.values(books).filter((book) => book.author===author);
-    if(book.length>0){
-        return res.status(200).json(book);
-    }else{
-        return res.status(400).json({message: "Book not found, given author: "+author});
-    }
-});
+    
+    axios.get('https://andres-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/db')
+        .then(response => {
+            books=response.data
+            let book=Object.values(books).filter((book) => book.author===author);
+            if(book.length>0){
+                return res.status(200).json(book);
+            }else{
+                return res.status(400).json({message: "Book not found, given author: "+author});
+            }
+        })
+        .catch(error => {
+            return res.status(500).json({message: "Internal error at getting db", error: error});
+        });
+ });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let title=req.params.title;
-    let book=Object.values(books).filter((book) => book.title===title);
-    if(book.length>0){
-        return res.status(200).json(book);
-    }else{
-        return res.status(400).json({message: "Book not found, given title: "+title});
-    }
+    axios.get('https://andres-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/db')
+        .then(response => {
+            books=response.data
+            let book=Object.values(books).filter((book) => book.title===title);
+            if(book.length>0){
+                return res.status(200).json(book);
+            }else{
+                return res.status(400).json({message: "Book not found, given title: "+title});
+            }
+        })
+        .catch(error => {
+            return res.status(500).json({message: "Internal error at getting db", error: error});
+        });
 });
 
 //  Get book review
